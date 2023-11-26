6387fe507a48c7670565002d08bd29eecdd1e6f1
iverly <github@iverly.net>
2023-11-26 21:51:25 +0100

    feat: use fastify instead of express
    Signed-off-by: iverly <github@iverly.net>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,17 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0');
