6dd76f4b4c021648f71e6163482d8730747ec480
Simon Lucido <simon34280@gmail.com>
2024-01-29 20:50:50 +0100

    feat: retries on db connection failure
    Signed-off-by: Simon Lucido <simon34280@gmail.com>

--- packages/database/src/lib/prisma.service.ts
+++ packages/database/src/lib/prisma.service.ts
@@ -1,9 +1,20 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
-    await this.$connect();
+    for (let i = 0; i < 10; i++) {
+      try {
+        await this.$connect();
+        return;
+      } catch (e) {
+        this.logger.error(`Failed to connect to database (${i})`);
+        this.logger.error(e);
+        await new Promise((r) => setTimeout(r, 1000));
+      }
+    }
   }
 }
